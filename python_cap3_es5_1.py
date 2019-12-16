bd01ca1ae306facd63217d068a6d9a783b6dac63
Claudiu Varvarici <47360403+claudiuvarvarici@users.noreply.github.com>
2019-12-16 16:25:37 +0100

    Create python_cap3_es5_1.py

---
+++ python_cap3_es5_1.py
@@ -0,0 +1,12 @@
+def beams():
+  print('+', '--', '--', '--','+', '--', '--', '--', '+')
+
+def posts():
+  print('|', '  ', '  ', '  ','|', '  ', '  ', '  ', '|')
+
+def grid():
+  beams(), posts(), posts(), posts(),
+  beams(), posts(), posts(), posts(),
+  beams()
+
+grid()
