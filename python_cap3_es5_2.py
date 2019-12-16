37947f60639f719324a3edb7ab32e9a2be2adb09
Claudiu Varvarici <47360403+claudiuvarvarici@users.noreply.github.com>
2019-12-16 16:23:47 +0100

    Create python_cap3_es5_2.py

---
+++ python_cap3_es5_2.py
@@ -0,0 +1,14 @@
+def beams():
+  print('+', '--', '--', '--','+', '--', '--', '--', '+', '--', '--', '--','+', '--', '--', '--', '+')
+
+def posts():
+  print('|', '  ', '  ', '  ','|', '  ', '  ', '  ', '|', '  ', '  ', '  ','|', '  ', '  ', '  ', '|')
+
+def grid():
+  beams(), posts(), posts(), posts(), posts(), posts(),
+  beams(), posts(), posts(), posts(), posts(), posts(),
+  beams(), posts(), posts(), posts(), posts(), posts(),
+  beams(), posts(), posts(), posts(), posts(), posts(), 
+  beams()
+
+grid()
