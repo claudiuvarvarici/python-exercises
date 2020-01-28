3737dc9eb385fbf99ac736e806d15155e72a9180
Claudiu Varvarici <47360403+claudiuvarvarici@users.noreply.github.com>
2020-01-28 10:42:58 +0100

    Create list.py

---
+++ list.py
@@ -0,0 +1,10 @@
+a=[1, 4, 9, 16, 25, 36, 49]
+b=['alpha', 'bravo', 'charlie', 'delta', 'echo']
+c=['zebra', 49, -879, 'aardvark', 200]
+
+def lista_numeri(l):
+  for x in range(len(l)):
+    print(l[x])
+    #print(l)
+
+lista_numeri(c)
